8054a3a4735c072f53ef6d452559e2d8b168ae80
Held Grijo <held.grijo@gmail.com>
2020-05-26 19:29:46 -0400

    Inserindo elementos da página de login

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,6 +1,20 @@
 import React from "react";
 import "./styles.css";
-
+import logoImg from "../../assets/logo.svg";
+import heroesImg from "../../assets/heroes.png";
 export default function Logon() {
-  return <h1>Hello Logon</h1>;
+  return (
+    <div className="logon-container">
+      <section className="form">
+        <img src={logoImg} alt="Be The hero" />
+        <form action="">
+          <h1>Faça seu Logon</h1>
+          <input placeholder="Sua ID" />
+          <button type="submit">Entrar</button>
+          <a href="/register">Não tenho cadastro</a>
+        </form>
+      </section>
+      <img src={heroesImg} alt="heroes" />
+    </div>
+  );
 }
