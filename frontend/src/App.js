fabe7abda16f24074f1e414678c30df95171a3cc
Held Grijo <held.grijo@gmail.com>
2020-05-26 18:56:32 -0400

    Aplicando estilos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,8 @@
-import React, { useState } from "react";
-import Header from "./Header";
+import React from "react";
+import Logon from "./pages/Logon";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  function increment() {
-    setCounter(counter + 1);
-  }
-
-  return (
-    <div>
-      <Header>Contador: {counter}</Header>
-      <button onClick={increment}>Incrementar</button>
-    </div>
-  );
+  return <Logon />;
 }
 
 export default App;
