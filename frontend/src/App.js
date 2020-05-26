69a8d8e6e4f94d3ec286a6e773a6002c37272c91
Held Grijo <held.grijo@gmail.com>
2020-05-26 14:22:03 -0400

    Usando desestruturação para captar propriedade específica

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./Header";
 
 function App() {
-  return <Header title="Semana Omnistack 11" />;
+  return <Header>Semana Omnistack 11</Header>;
 }
 
 export default App;
