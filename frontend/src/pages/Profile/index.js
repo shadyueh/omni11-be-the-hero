d7d9c7ab43d73abe78f5e65404278ddfd876ccd8
Held Grijo <held.grijo@gmail.com>
2020-05-26 23:18:53 -0400

    Estrutura do header da tela profile

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FiPower } from "react-icons/fi";
+import logoImg from "../../assets/logo.svg";
 
 export default function Profile() {
-  return <h1>Profile</h1>;
+  return (
+    <div className="profile-container">
+      <header>
+        <img src={logoImg} alt="Be The Hero" />
+        <span>Bem vindo, APAD</span>
+        <Link className="button" to="/incidents/new">
+          Cadastrar novo caso
+        </Link>
+        <button type="button">
+          <FiPower size={18} color="#e02041" />
+        </button>
+      </header>
+    </div>
+  );
 }
