96b25b476ea031fa355c52d23e6b5acbc30be1c1
Held Grijo <held.grijo@gmail.com>
2020-05-26 22:27:12 -0400

    Comportamento de SPA no link

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { FiLogIn } from "react-icons/fi";
 import "./styles.css";
 import logoImg from "../../assets/logo.svg";
@@ -14,10 +15,10 @@ export default function Logon() {
           <button className="button" type="submit">
             Entrar
           </button>
-          <a href="/register">
+          <Link to="/register">
             <FiLogIn size={16} color="#e02041" />
             Não tenho cadastro
-          </a>
+          </Link>
         </form>
       </section>
       <img src={heroesImg} alt="heroes" />
